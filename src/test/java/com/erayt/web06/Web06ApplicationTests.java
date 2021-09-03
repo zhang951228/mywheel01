59c9d20add8e732e573a59325860c2de67f888a8
Z151 <292700563@qq.com>
2021-09-03 11:50:22 +0800

    初始化

---
+++ src/test/java/com/erayt/web06/Web06ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erayt.web06;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Web06ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
